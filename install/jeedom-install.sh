1e7e2609311d5de01b3122b28a944bffbeb2cb89
Mips2648 <mips2648@gmail.com>
2025-03-07 09:29:21 +0100

    split steps

--- install/jeedom-install.sh
+++ install/jeedom-install.sh
@@ -23,11 +23,58 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 # Setup App
-msg_info "Setup ${APPLICATION}"
+msg_info "Downloading ${APPLICATION} installation"
 wget -q https://raw.githubusercontent.com/jeedom/core/master/install/install.sh
 chmod +x install.sh
-$STD ./install.sh
-msg_ok "Setup ${APPLICATION}"
+msg_ok "Downloaded installation script"
+
+msg_info "Upgrade OS"
+$STD ./install.sh -s 1
+msg_ok "Upgraded OS"
+
+msg_info "Install Jeedom main dependencies"
+$STD ./install.sh -s 2
+msg_ok "Installed Jeedom main dependencies"
+
+msg_info "Install Database"
+$STD ./install.sh -s 3
+msg_ok "Database installed"
+
+msg_info "Install Apache"
+$STD ./install.sh -s 4
+msg_ok "Apache installed"
+
+msg_info "Install PHP"
+$STD ./install.sh -s 5
+msg_ok "PHP installed"
+
+msg_info "Download Jeedom core"
+$STD ./install.sh -s 6
+msg_ok "Download done"
+
+msg_info "Database customisation"
+$STD ./install.sh -s 7
+msg_ok "Database customisation done"
+
+msg_info "Jeedom customisation"
+$STD ./install.sh -s 8
+msg_ok "Jeedom customisation done"
+
+msg_info "Configure Jeedom"
+$STD ./install.sh -s 9
+msg_ok "Jeedom configured"
+
+msg_info "Install Jeedom"
+$STD ./install.sh -s 10
+msg_ok "Jeedom installed"
+
+msg_info "Post installation"
+$STD ./install.sh -s 11
+msg_ok "Post installation done"
+
+msg_info "Check installation"
+$STD ./install.sh -s 12
+msg_ok "Installation checked, everything seems successfuly installed"
 
 motd_ssh
 customize
