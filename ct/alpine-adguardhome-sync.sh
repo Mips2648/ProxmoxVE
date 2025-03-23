cb3770f613ab3a08c862f86f9a502c2869e9637c
Mips2648 <mips2648@gmail.com>
2025-03-23 07:58:59 +0100

    init

---
+++ ct/alpine-adguardhome-sync.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: Mips
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/bakito/adguardhome-sync
+
+APP="Alpine-Adguardhome-Sync"
+var_tags="Adguardhome;alpine"
+var_cpu="1"
+var_ram="256"
+var_disk="0.5"
+var_os="alpine"
+var_version="3.21"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    if ! apk -e info newt >/dev/null 2>&1; then
+        apk add -q newt
+    fi
+    while true; do
+        CHOICE=$(
+            whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUPPORT" --menu "Select option" 11 58 1 \
+                "1" "Check for Adguardhome-Sync Updates" 3>&2 2>&1 1>&3
+        )
+        exit_status=$?
+        if [ $exit_status == 1 ]; then
+            clear
+            exit-script
+        fi
+        header_info
+        case $CHOICE in
+        1)
+            apk update && apk upgrade
+            exit
+            ;;
+        esac
+    done
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
---
+++ install/alpine-adguardhome-sync.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.docker.com/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+# $STD apk add newt
+$STD apk add curl
+# $STD apk add openssh
+# $STD apk add tzdata
+$STD apk add nano
+$STD apk add go
+# $STD apk add mc
+msg_ok "Installed Dependencies"
+
+get_latest_release() {
+  curl -sL https://api.github.com/repos/bakito/adguardhome-sync/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+LATEST_VERSION=$(get_latest_release)
+
+msg_info "Installing Adguardhome-Sync $LATEST_VERSION"
+$STD go install github.com/bakito/adguardhome-sync@latest
+msg_ok "Installed Adguardhome-Sync $LATEST_VERSION"
+
+motd_ssh
+customize
